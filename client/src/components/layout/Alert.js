bb288704d63e3e157232858333bb863c0c79379a
luigi <igiboi26@gmail.com>
2021-06-08 11:00:05 -0400

    I had to add alert into reduer/index.js into export default and added settimeout to remove the alert

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,14 +1,13 @@
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 const Alert = ({ alerts }) =>
-    alerts !== null &&
-    alerts.length > 0 &&
-  alerts.map((alerts) => (
-    <div key={alerts.id} className={`alert alert-${alerts.alertType}`}>
-      {alerts.msg}
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map(alert => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
     </div>
   ));
 
@@ -16,8 +15,8 @@ Alert.propTypes = {
   alerts: PropTypes.array.isRequired
 };
 
-const mapStateToProps = (state) => ({
-  alerts: state.alerts
+const mapStateToProps = state => ({
+  alerts: state.alert
 });
 
 export default connect(mapStateToProps)(Alert);
